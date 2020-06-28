33c6a6545a8f751842cf8ddc52b3841421d2f7cb
勝 <g18tka07@yamanashi.ac.jp>
2020-06-28 19:22:20 +0900

    revise delete.py

--- delete.py
+++ delete.py
@@ -5,25 +5,27 @@ from utils import file_tool
 
 gpu_num = os.environ['CUDA_VISIBLE_DEVICES']
 
-if len(gpu_num) != 1:
+gpu_num = gpu_num.split(',')
+if len(gpu_num) == 0:
     raise ValueError
 
 from socket import gethostname
 
-tensorboard_path = f'result/tensorboard/MTL_PI/{gethostname()}/gpu_{gpu_num}'
+for gpu_n in gpu_num:
+    tensorboard_path = f'result/tensorboard/MTL_PI/{gethostname()}/gpu_{gpu_n}'
 
-optuna_path = f'result/optuna/MTL_PI/{gethostname()}/gpu_{gpu_num}'
+    optuna_path = f'result/optuna/MTL_PI/{gethostname()}/gpu_{gpu_n}'
 
-auxiliary_path = 'framework/mtl/after_auxiliary/'
+    auxiliary_path = 'framework/mtl/after_auxiliary/'
 
-file_path = [auxiliary_path, tensorboard_path, optuna_path]
+    file_path = [auxiliary_path, tensorboard_path, optuna_path]
 
-for fp in file_path:
-    if file_tool.check_dir(fp):
-        str = os.popen(f"rm -r {fp}").read()
-        print(str)
-    else:
-        print(f'{fp} do not exist!')
+    for fp in file_path:
+        if file_tool.check_dir(fp):
+            str = os.popen(f"rm -r {fp}").read()
+            print(str)
+        else:
+            print(f'{fp} do not exist!')
 
 # a = str.split("\n")
 # for b in a:
