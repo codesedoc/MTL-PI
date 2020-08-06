d304f167c356490752794f6f220917359fb5344c
勝 <1270118646@qq.com>
2020-08-06 19:29:47 +0900

    x

--- framework/mtl/mtl_pi.py
+++ framework/mtl/mtl_pi.py
@@ -483,8 +483,8 @@ class MTLPIFrameworkProxy(TFRsFrameworkProxy):
     def args_need_to_record(self) -> Dict[str, Any]:
         result = {
             'transformer': f"{self.framework.transformer_type.name}: {self.model_args.model_name_or_path}",
-            'distance_type': self.model_args.distance_type,
-            'feature_compared': self.model_args.feature_compared,
+            # 'distance_type': self.model_args.distance_type,
+            # 'feature_compared': self.model_args.feature_compared,
             'chose_two_way_when_evaluate': self.chose_two_way_when_evaluate,
             'adjust_prediction': self.model_args.adjust_prediction,
             'single_task': self.model_args.single_task,
